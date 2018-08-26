c68fe588c8c7fc63aaca8707e9a8fff3020e3d86
SergeA <ronnieo@gmail.com>
2018-08-25 22:19:50 -0400

    Exercise completed

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActvity.java
@@ -0,0 +1,20 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+/**
+ * Created by angelov on 8/25/2018.
+ */
+
+public class MainActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
